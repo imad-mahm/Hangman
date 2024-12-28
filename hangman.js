0f3440ae4ae785e00bdefcc06e5f1c9e7a969e98
imad-mahm <144839563+imad-mahm@users.noreply.github.com>
2024-12-28 13:06:41 +0200

    Add files via upload

--- hangman.js
+++ hangman.js
@@ -3,6 +3,7 @@ const submitButton = document.getElementById('submit');
 var hangSteps = document.getElementById('hangman');
 var hangWord = document.getElementById('word');
 var notif = document.getElementById('notif');
+var wrong = document.getElementById("wrong-guesses");
 const hangmanWords = ['hello', 'dog', 'cat', 'bird', 'fish', 'car', 'tree', 'house', 'phone',
                         'computer', 'misery', 'happiness', 'dragon', 'dinosoar', 'clock', 'apple', 
                         'battery', 'host', 'science', 'programming', 'biology', 'economy', 'money',
@@ -13,6 +14,7 @@ let guessedindices = new Set();
 let prevSize = 0;
 let step = 1;
 let gameActive = true;
+let wrongGuesses = [];
 
 function guess() {
     if (!gameActive) return;
@@ -32,29 +34,42 @@ function guess() {
 
             for (let i = 0; i < wordArray.length; i++) {
                 if (wordArray[i] === letter) {
+                    if(guessedindices.has(i)) {
+                        notif.innerHTML = ('You already guessed this letter');
+                        inputBox.value = '';
+                        return;
+                    }
                     guessedindices.add(i);
+
                 }
             }
 
             if (guessedindices.size <= prevSize) {
+                if(wrongGuesses.indexOf(letter)>-1){
+                    notif.innerHTML = ('You already tried this wrong letter');
+                    inputBox.value = '';
+                    return; 
+                }
                 hangSteps.src = hangman[step];
                 step++;
                 notif.innerHTML = ('Wrong guess');
+                wrongGuesses.push(letter);
                 if(step>8) {
                     notif.innerHTML = ("you lost. resetting in 3 seconds...");
                     gameActive = false;
                     setTimeout(resetGame, 3000);
                 }
                 inputBox.value = '';
+                wrong.innerHTML = String(wrongGuesses);
                 return;
             }
             notif.innerHTML = "";
             let result = '';
             for (let i = 0; i < wordArray.length; i++) {
                 if (guessedindices.has(i)) {
-                    result += wordArray[i];
+                    result += wordArray[i] +" ";
                 } else {
-                    result += '_';
+                    result += '_ ';
                     guessed = false;
                 }
             }
@@ -70,18 +85,20 @@ function guess() {
         }
     }
     inputBox.value = '';
+    wrong.innerHTML = String(wrongGuesses);
 }
 
 function resetGame(){
     step = 1;
     hangSteps.src = hangman[0];
     index = Math.floor(Math.random() * hangmanWords.length);
-    hangWord.innerHTML = "_".repeat(hangmanWords[index].length);
+    hangWord.innerHTML = "_ ".repeat(hangmanWords[index].length);
     guessedindices = new Set();
     prevSize = 0;
     notif.innerHTML = '';
     gameActive = true;
     inputBox.value = "";
+    wrongGuesses = [];
 }
 
 resetGame();
